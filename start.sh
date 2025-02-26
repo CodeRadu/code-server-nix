659b0ab1fc92f665d2ed2c429dfe8bae22256829
CodeRadu <47945947+CodeRadu@users.noreply.github.com>
2025-02-26 21:19:34 +0200

    Simplify direnv installation in start.sh by adding hook only if .bashrc is missing

--- start.sh
+++ start.sh
@@ -1,21 +1,9 @@
 mkdir -p /root/.config/code-server
 
-cat > /root/.bashrc <<EOF
-_direnv_hook() {
-  local previous_exit_status=$?;
-  trap -- '' SIGINT;
-  eval "$("/nix/store/irq2506ybbvsnpmb5gflcbg90390bmdg-direnv-2.35.0/bin/direnv" export bash)";
-  trap - SIGINT;
-  return $previous_exit_status;
-};
-if [[ ";${PROMPT_COMMAND[*]:-};" != *";_direnv_hook;"* ]]; then
-  if [[ "$(declare -p PROMPT_COMMAND 2>&1)" == "declare -a"* ]]; then
-    PROMPT_COMMAND=(_direnv_hook "${PROMPT_COMMAND[@]}")
-  else
-    PROMPT_COMMAND="_direnv_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
-  fi
+if [ ! -f /root/.bashrc ]; then
+  # Install the direnv hook
+  direnv hook bash > /root/.bashrc
 fi
-EOF
 
 cat > /root/.config/code-server/config.yaml <<EOF
 bind-addr: 0.0.0.0:${PORT}
